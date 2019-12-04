1bdb013350ee299d535575db77f64cc622a4a6c6
dansimco <ds@dansim.co>
2019-12-04 11:58:53 +0700

    add Ack sample engine

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ k3:  save meadowphysics data to `data/mp.data`
 k3^: save scales data to  `data/gridscales.data`
 e2:  change root note
 e3:  bpm change
+
+
 ```
--- meadowphysics.lua
+++ meadowphysics.lua
@@ -18,29 +18,16 @@ local clk = Beatclock.new()
 local meadowphysics = include("meadowphysics/lib/engine/core")()
 local g = grid.connect()
 
+local Ack = include("ack/lib/ack")
+engine.name = 'Ack'
 
-function init()
 
-  crow.ii.pullup(true)
-  crow.ii.jf.mode(1)
-
-  meadowphysics.init(8)
-  meadowphysics.on_bang = handle_bang
-
-  clk.on_step = function ()
-    meadowphysics:handle_tick()
-    meadowphysics.should_redraw = true
-    g:all(0)
-  end
-  clk:bpm_change(120)
-  clk:start()
-  redraw()
-end
 
 function handle_bang(e) -- Sound making thing goes here!
   if e.type == 'trigger' then
-    --print("TRIGGER", e.voice)
+    print("TRIGGER", e.voice)
     -- crow.ii.jf.play_note(e.voice/12 - 37/1200,8)
+    engine.trig(e.voice-1)
   end
   if e.type == 'gate' and e.value == 1 then
     -- print("GATE HIGH", e.voice)
@@ -50,6 +37,65 @@ function handle_bang(e) -- Sound making thing goes here!
   end
 end
 
+
+
+
+function init()
+
+  crow.ii.pullup(true)
+  crow.ii.jf.mode(1)
+
+  meadowphysics.init(8)
+  meadowphysics.on_bang = handle_bang
+
+
+
+  params:add_separator()
+  params:add_separator()
+  Ack.add_params()
+  for i=1, meadowphysics.voice_count do
+    Ack.add_channel_params(i)
+  end
+
+  print(engine.list_commands())
+
+  clk.on_step = function ()
+    meadowphysics:handle_tick()
+    meadowphysics.should_redraw = true
+    g:all(0)
+  end
+  clk:bpm_change(120)
+  clk:start()
+
+  -- Test properties
+  meadowphysics.voices[1].is_playing = true
+  meadowphysics.voices[1].target_voices = {
+    meadowphysics.voices[1],
+    meadowphysics.voices[2]
+  }
+  meadowphysics.voices[1].ticks_per_step = 1
+  meadowphysics.voices[2].ticks_per_step = 1  
+  meadowphysics.voices[4].is_playing = true
+  meadowphysics.voices[4].target_voices = {
+    meadowphysics.voices[4],
+    meadowphysics.voices[5]
+  }
+  meadowphysics.voices[4].ticks_per_step = 2
+  meadowphysics.voices[5].ticks_per_step = 2
+  meadowphysics.voices[7].is_playing = true
+  meadowphysics.voices[7].target_voices = {
+    meadowphysics.voices[7],
+    meadowphysics.voices[8]
+  }
+  meadowphysics.voices[7].ticks_per_step = 4
+  meadowphysics.voices[8].ticks_per_step = 4
+
+
+
+
+  redraw()
+
+end
 
 function enc()
   meadowphysics:handle_enc()
