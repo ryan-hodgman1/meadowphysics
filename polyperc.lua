25e8fa1ec1a4cf57228cde71379590b08e46ffd7
dansimco <ds@dansim.co>
2020-03-12 12:54:09 +0700

    add polyperc params

--- polyperc.lua
+++ polyperc.lua
@@ -74,7 +74,43 @@ function init()
 
   scale:make_params()
 
-  -- meadowphysics.init(16)
+  -- Engine Params
+  cs_AMP = controlspec.new(0,1,'lin',0,0.5,'')
+  params:add{
+    type="control",id="amp",controlspec=cs_AMP,
+    action=function(x) engine.amp(x) end
+  }
+
+  cs_PW = controlspec.new(0,100,'lin',0,50,'%')
+  params:add{
+    type="control",id="pw",controlspec=cs_PW,
+    action=function(x) engine.pw(x/100) end
+  }
+
+  cs_REL = controlspec.new(0.1,3.2,'lin',0,1.2,'s')
+  params:add{
+    type="control",id="release",controlspec=cs_REL,
+    action=function(x) engine.release(x) end
+  }
+
+  cs_CUT = controlspec.new(50,5000,'exp',0,800,'hz')
+  params:add{
+    type="control",id="cutoff",controlspec=cs_CUT,
+    action=function(x) engine.cutoff(x) end
+  }
+
+  cs_GAIN = controlspec.new(0,4,'lin',0,1,'')
+  params:add{
+    type="control",id="gain",controlspec=cs_GAIN,
+    action=function(x) engine.gain(x) end
+  }
+  
+  cs_PAN = controlspec.new(-1,1, 'lin',0,0,'')
+  params:add{
+    type="control",id="pan",controlspec=cs_PAN,
+    action=function(x) engine.pan(x) end
+  }
+
   meadowphysics.init(8)
   meadowphysics.on_bang = handle_bang
   meadowphysics.clock = clk
@@ -136,5 +172,46 @@ function cleanup ()
   oled_r:stop()
   clk:stop()
 end
+
+
+function voice_params()
+
+  params:add {
+    type = "control",
+    id = "amp",
+    controlspec = controlspec.new(0,1,'lin',0,0.5,''),
+    action = function(x) engine.amp(x) end
+  }
+
+  params:add {
+    type = "control",
+    id = "pw",
+    controlspec = controlspec.new(0,100,'lin',0,50,'%'),
+    action = function(x) engine.pw(x/100) end
+  }
+
+  params:add {
+    type = "control",
+    id = "release",
+    controlspec = controlspec.new(0.1,3.2,'lin',0,1.2,'s'),
+    action = function(x) engine.release(x) end
+  }
+
+  params:add {
+    type = "control",
+    id = "cutoff",
+    controlspec = controlspec.new(50,5000,'exp',0,555,'hz'),
+    action = function(x) engine.cutoff(x) end
+  }
+
+  params:add {
+    type = "control",
+    id = "gain",
+    controlspec = controlspec.new(0,4,'lin',0,1,''),
+    action = function(x) engine.gain(x) end
+  }
+
+end
+
 
 
